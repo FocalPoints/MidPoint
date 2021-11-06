8f176f6fb5e6d6bf1ee522355150a0d02906d938
YogiPaturu <ypaturu@gmail.com>
2021-11-06 15:56:24 -0500

    Beginning Main page

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Map from './Map'
+import Sidebar from './Sidebar'
 
 const Main = (props) => {
-    return(<div>Logged in</div>)
+    return(
+      <div>
+        Logged in
+        <Sidebar />
+        <Map />
+      </div>
+    )
 }
 
 export default Main;
--- client/reducers/mainPageReducer.js
+++ client/reducers/mainPageReducer.js
@@ -4,12 +4,19 @@ import axios from 'axios';
 
 const initialState = {
   pageToDisplay: 'login',
+  currentUserID: '', // this is primary key for the username, should be a number.
   loggedIn: false
 };
 
 const mainPageReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.SIGN_UP:
+      // const body = {
+      //   path: '/login',
+      //   method: POST,
+      //   body: action.payload, // {user,pass, {lat,lng}}
+      // }
+      // axios.post(/(server endpoint), body).then(data => {})
       return {
         ...state,
         pageToDisplay: 'signup',
@@ -26,16 +33,20 @@ const mainPageReducer = (state = initialState, action) => {
       // const body = {
       //   path: '/login',
       //   method: GET,
-      //   body: action.payload,
+      //   body: action.payload, // {user,pass}
       // }
+
       // axios.get(/(server endpoint), body).then(data => {})
 
       // DB res back with all of data, use the data to change the state.
       // return statement should go inside the .then
+      //server will res back with an object that has a boolean value and a msg
       return {
         ...state,
-        loggedIn: true,
+        loggedIn: true, // obj.booleanValue
       };
+
+      //update location needs ID
 
     default:
       return state;
