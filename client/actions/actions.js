cd3cd9ca9a91da6de3d5b08b9b8c5ee9455bbb9b
CecilyJ <cecilyjansen@gmail.com>
2021-11-11 00:08:46 -0500

    organized code and refactored initialState, reducer, actions, and props

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,30 +1,24 @@
 import * as types from '../constants/actionTypes';
 import axios from 'axios';
 
-export const logIn = (username, password) => (dispatch) => {
-  const request = {
-    method: 'GET',
-    url: '/database/login',
-    params: {username, password}
-  }
- 
-  axios.request(request).then((response) => {
-    if(response.status = 201) dispatch({
-      type: types.LOG_IN,
-      payload: response.data, //will hold the user object
-    });
-  }).catch(console.error);
-};
+
+
+export const pageToSignup = () => ({
+  type: types.PAGE_TO_SIGN_UP,
+});
+
+export const signUpCancel = () => ({
+  type: types.SIGN_UP_CANCEL,
+});
 
 export const logOut = () => ({
   type: types.LOG_OUT,
 });
 
-
-
-
-export const pageToSignup = () => ({
-  type: types.PAGE_TO_SIGN_UP,
+export const updateLocation = (newAddress) => ({
+  //update to also request that the user coords get updated, send back updated coords and new displayAddress
+  type: types.UPDATE_LOCATION,
+  payload: {newAddress},
 });
 
 export const signUpUser = (username, password, address) => (dispatch) => {
@@ -44,26 +38,20 @@ export const signUpUser = (username, password, address) => (dispatch) => {
     }).catch(console.error);
 };
 
-
-export const signUpCancel = () => ({
-  type: types.SIGN_UP_CANCEL,
-});
-
-export const updateLocation = (address) => ({
-  type: types.UPDATE_LOCATION,
-  payload: {address},
-})
-
-export const getMidpoint = (userCoords, friendCoords) => {
-
-  const lat = (userCoords.lat + friendCoords.lat) / 2;
-  const lng = (userCoords.lng + friendCoords.lng) / 2;
-  
-  return ({
-    type: types.GET_MIDPOINT,
-    payload:  {'lat': lat, 'lng': lng}
-  })
-}
+export const logIn = (username, password) => (dispatch) => {
+  const request = {
+    method: 'GET',
+    url: '/database/login',
+    params: {username, password}
+  }
+ 
+  axios.request(request).then((response) => {
+    if(response.status = 201) dispatch({
+      type: types.LOG_IN,
+      payload: response.data, //will hold the user object
+    });
+  }).catch(console.error);
+};
 
 export const addFriend = (user1_id, user2_id) => (dispatch) => {
   const request = {
@@ -80,35 +68,13 @@ export const addFriend = (user1_id, user2_id) => (dispatch) => {
   }).catch(console.error);
 }
 
+export const getMidpoint = (userCoords, friendCoords) => {
 
-
-// export const deleteCard = id => (dispatch, getState) => {
-//   if (getState().markets.marketList[id].cards > 0) {
-//     dispatch({ type: types.DELETE_CARD, payload: id });
-//   }
-// };
-
-
-
-
-// export const buyStock = () => (dispatch, getState) => {
-//   // grab the symbol from state
-//   const sym = getState().stocks.searchBar;
-//   const user_id = getState().stocks.user_id;
-//   // hit API to get current price of stock
-//   const options = {
-//     method: 'POST',
-//     url: '/transaction',
-//     data: {user_id},
-//     params: {region: 'US', symbols: sym}
-//   }
-//   // make a post to the stocks database adding the stock name and current price
-//   // make a post to the transactions database with the current user and the stock ID and time, sold price/sold time null
-//   axios.request(options).then((response) => {
-//     if(response.status = 201) dispatch({
-//       type: types.BUY_STOCK,
-//       payload: response.data,
-//     });
-//   }).catch(console.error);
-//   // hit reducer to update state with the new query of all transactions...
-// };
+  const lat = (userCoords.lat + friendCoords.lat) / 2;
+  const lng = (userCoords.lng + friendCoords.lng) / 2;
+  
+  return ({
+    type: types.GET_MIDPOINT,
+    payload:  {'lat': lat, 'lng': lng}
+  })
+}
