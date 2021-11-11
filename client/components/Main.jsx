25096fe23dffcfdd4326bd070073e21ab3bdd881
Terence <89863790+TERR-inss@users.noreply.github.com>
2021-11-10 19:45:11 -0500

    Re-creating Main.jsx
    miss-steak

---
+++ client/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Map from './Map'
+import Sidebar from './Sidebar'
+
+const Main = (props) => {
+    return(
+      <div id="main-content" className='mainStyles'>
+        <Sidebar {...props}/>
+        <Map {...props}/>
+      </div>
+    )
+}
+
+export default Main;
