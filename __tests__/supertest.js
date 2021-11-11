0c41a3ad0d1386d15ba7f004379a870436d7207d
TERR-inss <robo.terence@gmail.com>
2021-11-11 16:31:32 -0500

    further supertest additions to test post requests

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -15,7 +15,7 @@ describe('Server Tests', () => {
     });
 
     describe('/database/login', () => {
-        describe('GET - without login', () => {
+        describe('GET - with invalid login', () => {
             it('if no login info provided, responds with 404 status and application/json content type', () => {
                 return request(server)
                     .get('/database/login')
@@ -31,7 +31,7 @@ describe('Server Tests', () => {
             });            
         });
 
-        describe('GET - successful login', () => {
+        describe('GET - with valid login', () => {
             it('provided with *valid* login, responds with 200 status and application/json content type', () => {
                 return request(server)
                     .get('/database/login?username=terence&password=petersen')
@@ -56,27 +56,61 @@ describe('Server Tests', () => {
                         expect(Array.isArray(res.body.friendList)).toBe(true);
                 });
             });
+
+            it('--returned friendList array contains objects', () => {
+                return request(server)
+                    .get('/database/login?username=terence&password=petersen')
+                    .then(res => {
+                        const { friendList } = res.body;
+                        expect(friendList).toBeDefined();
+                        expect(typeof friendList[0] === 'object' && !Array.isArray(friendList[0])).toBe(true);
+                });
+            });
         });
     });
 
+    describe('/database/signup', () => {
+        // both of the below tests fail.
+        // need to determine if it is the tests that are written incorrectly
+        // or if the application is not behaving as expected
+        describe('POST - with invalid user/password input(s)', () => {
 
-    xdescribe('/database/signup', () => {
-        describe('POST - when user provides invalid user/password input(s)', () => {
+            // shows the server returning 500 "Internal Server Error", unlike the response shown in the browser
             it('returns status 401', () => {
                 return request(server)
-                    .post('/database/signup/')
-                    .send({ username: '', password: '', address: '' })
-                        .expect(401)
+                    .post('/database/signup')
+                    .send({ username: '', password: '', address: '123 fake street' })
+                        .expect(401);
             });
 
+            // the console log shows res.body to be an empty object
             it('returns "Invalid username and/or password!" message', () => {
                 return request(server)
-                    .post('/database/signup/')
-                    .send({ username: '', password: '', address: '' })
-                        .then(res => {
-                            expect(res.body.message).toEqual('Invalid username and/or password!');
-                        });
+                    .post('/database/signup')
+                    .send({ username: '', password: '', address: '123 fake street' })
+                    .then(res => {
+                        console.log(res.body);
+                        expect(res.body.message).toBe('Invalid username and/or password!');
+                });
             });
         });
+
+        describe('POST - with invalid address input', () => {
+            it ('returns status 500', () => {
+                return request(server)
+                    .post('/database/signup')
+                    .send({ username: 'newguy3', password: 'okiedokie', address: 'gjeirogrj' })
+                        .expect(500);
+            });
+        });
+
+        describe('POST - with valid inputs', () => {
+            it ('returns status 200', () => {
+                return request(server)
+                    .post('/database/signup')
+                    .send({ username: 'newguy4', password: 'okiedokie', address: '123 fake street' })
+                        .expect(201);
+            })
+        })
     });
 });
